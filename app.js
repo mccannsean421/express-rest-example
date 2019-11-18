695a4a9fc3ca73ec0c5a08365218ce0b3dd3643c
Cormac McCann <cormacmccann@Cormacs-MacBook-Pro.local>
2019-11-18 11:21:14 +0000

    added CRUD operations

--- app.js
+++ app.js
@@ -2,9 +2,22 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+//Models
+const Post = require("./models/post");
 
 const app = express();
 const port = 8080;
+
+// DB Connection
+
+mongoose.connect('mongodb://mccann:password1@ds061928.mlab.com:61928/venm-example');
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error"));
+db.once("open", function(callback){
+  console.log("Connection Succeeded");
+});
 
 var corsOptions = {
   origin: 'http://localhost:8081',
@@ -23,13 +36,88 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get('/posts', cors(), (req, res) => {
-  res.json([
-    {
-      title: 'post 1',
-      description: 'Description of Post 1'
+// Create Post
+app.post('/posts', (req, res) => {
+  let db = req.db;
+  let title = req.body.title;
+  let description = req.body.description;
+
+  console.log(req.body);
+
+  let newPost = new Post({
+    title: title,
+    description: description,
+  });
+
+  newPost.save((error) => {
+    if (error) {
+      console.log(error);
     }
-  ])
+
+    res.send({
+      success: true,
+      message: 'Post saved!'
+    })
+  })
+
+})
+
+
+// Get Posts
+app.get('/posts', cors(), (req, res) => {
+  Post.find({}, 'title description', (error, posts) => {
+    if (error) {
+      console.error(error);
+    }
+
+    res.send({
+      posts: posts
+    })
+  }).sort({_id:-1});
+});
+
+// Get a Single Post
+app.get('/posts/:id', (req, res) => {
+  let db = req.db;
+
+  Post.findById(req.params.id, 'title description', (error, post) => {
+    if (error) console.error(error);
+    res.send(post);
+  })
+})
+
+// Get a Single Post
+app.put('/posts/:id', (req, res) => {
+  let db = req.db;
+
+  Post.findById(req.params.id, 'title description', (error, post) => {
+    if (error) console.error(error);
+
+    post.title = req.body.title;
+    post.description  = req.body.description;
+    post.save((error) => {
+      if (error) console.error(error);
+
+      res.send({
+        success: true
+      });
+    })
+  })
+});
+
+// Delete Post
+app.delete('/posts/:id', (req, res) => {
+  let db = req.db;
+
+  Post.remove({
+    _id: req.params.id
+  }, (err, post) => {
+    if (err) res.send(err);
+
+    res.send({
+      success: true
+    })
+  })
 })
 
 app.listen(port, () => {
